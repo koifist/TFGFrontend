659402ff50c1824fada5d46d43c76e1154538b60
Fernando Gonzalez <ferjgm1996@gmail.com>
2019-09-19 21:52:12 +0200

    Set prod environmnet

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+const BASE_URL = 'http://realtimestockanalysis:3000';
 export const environment = {
-  production: true
+  production: true,
+  backendpoints: {
+    login: BASE_URL + '/createSession',
+    register: BASE_URL + '/createUser',
+    updatePass: BASE_URL + '/updatePass',
+    getMessages: BASE_URL + '/getMessages',
+    sendMessage: BASE_URL + '/sendMessage',
+    upgradeBroker: BASE_URL + '/createBroker'
+
+  },
+  baseurl: 'http://realtimestockanalysis:3000'
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
